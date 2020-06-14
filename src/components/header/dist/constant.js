9f212f568e7817ecae56ccda9a4bceef796d63ba
serveyk0 <ssimpl8ss@gmail.com>
2020-06-14 11:37:21 +0300

    create header

--- src/components/header/dist/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.search = exports.work_day = exports.address = exports.phone_2 = exports.phone_1 = void 0;
-exports.phone_1 = "+38 (066) 929-71-18;";
-exports.phone_2 = "+38 (066) 929-71-18;";
-exports.address = "109147, г. Москва, улица Марксистская, дом 34, корпус 4, 1 этаж";
-exports.work_day = "пн-пт 9:00-18:00";
-exports.search = "Поиск";
