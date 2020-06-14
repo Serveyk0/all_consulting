2d04092f67270b66b0dbae7679cb441557c77503
serveyk0 <ssimpl8ss@gmail.com>
2020-06-14 14:56:02 +0300

    create footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ooo, description } from "./constants";
+import "./footer.sass";
 
 export const Footer: React.FC = ( ): JSX.Element=>  
 {
 
     return (
         <footer className="footer">
-            
+            <p className="footer_ooo">
+                {ooo}
+            </p>
+            <p className="footer_description">
+                {description}
+            </p>
         </footer>
     )
 }
